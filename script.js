55895d9e51eb65ddcf58ae13b5682b6ff21789ba
faunhub <76247482+faunhub@users.noreply.github.com>
2022-01-15 18:55:49 +0700

    almost done

--- script.js
+++ script.js
@@ -9,28 +9,58 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection == 'rock') {
             return 0;
         } else if (computerSelection == 'paper') {
-            console.log('Player lost. Paper beats Rock.');
+            result.innerText = "player lost. paper beats rock.";
+            comp.src = "images/paper.png";
+            setTimeout(() => {
+                comp.src="images/make_choice.gif";
+                result.innerText = "";}
+                , 700);
             return -1;
         } else {
-            console.log('Player won. Rock beats Scissors.');
+            result.innerText = "player won. rock beats scissors.";
+            comp.src = "images/scissors.png";
+            setTimeout(() => {
+                comp.src="images/make_choice.gif";
+                result.innerText = "";}
+                , 700);
             return 1;
         }
     } else if (playerSelection == 'paper') {
         if (computerSelection == 'rock') {
-            console.log('Player won. Paper beats Rock.');
+            result.innerText = "player won. paper beats rock.";
+            comp.src = "images/rock.png";
+            setTimeout(() => {
+                comp.src="images/make_choice.gif";
+                result.innerText = "";}
+                , 1300);
             return 1;
         } else if (computerSelection == 'paper') {
             return 0;
         } else {
-            console.log('Player lost. Scissors beat Paper.');
+            result.innerText = "player lost. scissors beat paper.";
+            comp.src = "images/paper.png";
+            setTimeout(() => {
+                comp.src="images/make_choice.gif";
+                result.innerText = "";}
+                , 700);
             return -1;
         }
     } else if (playerSelection == 'scissors') {
         if (computerSelection == 'rock') {
-            console.log('Player lost. Rock beats Scissors.');
+            result.innerText = "player lost. rock beats scissors.";
+            comp.src = "images/rock.png";
+            setTimeout(() => {
+                comp.src="images/make_choice.gif";
+                result.innerText = "";}
+                , 700);
             return -1;
         } else if (computerSelection == 'paper') {
-            console.log('Player won. Scissors beat Paper.');
+            result.innerText = "player won. scissors beat paper.";
+            comp.src = "images/paer.png";
+            setTimeout(() => {
+                comp.src="images/make_choice.gif";
+                result.innerText = "";}
+                , 700);
             return 1;
         } else {
             return 0;
@@ -39,48 +69,54 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game(){
-    
-    let playerSelection = this.textContent;
-    let computerSelection = computerPlay();
-    console.log(playerSelection);
-    console.log(computerSelection);
-    let result = playRound(playerSelection, computerSelection);
-        
-    if (result == 1) {
-        playerScoreCount++;
-        playerScore.textContent = playerScoreCount;
-    } else if (result == -1) {
-        computerScoreCount++;
-        computerScore.textContent = computerScoreCount;
-    } else if (result == 0){
-        comp.src = "images/tie.png";
-        setTimeout(() => comp.src="images/make_choice.gif", 500);
+    if (!playing){
+        return;
+    } else {
+        let playerSelection = this.textContent;
+        let computerSelection = computerPlay();
+        console.log(playerSelection);
+        console.log(computerSelection);
+        let result = playRound(playerSelection, computerSelection);
+            
+        if (result == 1) {
+            playerScoreCount++;
+            playerScore.firstElementChild.textContent = playerScoreCount;
+        } else if (result == -1) {
+            computerScoreCount++;
+            computerScore.firstElementChild.textContent = computerScoreCount;
+        } else if (result == 0){
+            comp.src = "images/tie.png";
+            setTimeout(() => comp.src="images/make_choice.gif", 700);
+        }
+        console.log ('Player: ' + playerScoreCount);
+        console.log ('Computer: ' + computerScoreCount);
     }
-    console.log ('Player: ' + playerScoreCount);
-    console.log ('Computer: ' + computerScoreCount);
 }
 
 function start(){
     playing = true;
     playerScoreCount =0;
     computerScoreCount =0;
+    playerScore.firstElementChild.textContent = 0;
+    computerScore.firstElementChild.textContent = 0;
     comp.src = "images/computer_default.png";
 }
-
 
 const player = document.querySelectorAll('.choice');
 const comp = document.querySelector('.computer');
 const restart = document.querySelector('.restart');
+const result = document.getElementById('result');
 let playing = false;
 comp.addEventListener('click', start);
 comp.addEventListener('click', () => comp.src = "images/make_choice.gif");
 restart.addEventListener('click', start);
+restart.addEventListener('click', () => playing =false)
 
 player.forEach(choice => choice.addEventListener('click', game));
 let playerScoreCount =0;
 let computerScoreCount =0;
-let playerScore = document.getElementById('playerScore');
-let computerScore =document.getElementById('computerScore');
+const playerScore = document.getElementById('playerScore');
+const computerScore =document.getElementById('computerScore');
 
 
 
